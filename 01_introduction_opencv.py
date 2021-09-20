6a326165ba727bb346da33e050ea1e1b4f5bc2fc
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-09-20 16:52:44 +0500

    1.0

---
+++ 01_introduction_opencv.py
@@ -0,0 +1,10 @@
+# (01)****************************Introduction to OpenCV in machine learning*******************************
+
+# -----Introduction----OpenCV is a library of programming functions mainly aimed at real-time computer vision. Originally developed by Intel, it was later supported by Willow Garage then Itseez. The library is cross-platform and free for use under the open-source Apache 2 License.
+
+# ------Size: ~200 MB
+# ------License: Apache license
+# ------Original author(s): Intel Corporation, Willow Garage, Itseez
+# ------Initial release: June 2000; 21 years ago
+# ------Stable release: 4.5.3 / 5 July 2021; 2 months ago
+# ------Written in: C/C++
---
+++ 02_using_anaconda_introduction.py
@@ -0,0 +1,19 @@
+# (02)****************************Anaconda introduction in Deep Learning and Machine Learning***********
+
+import sys
+import cv2
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import sklearn
+
+# ----check the versions of libraries
+print("openCV version : ", cv2.__version__)
+print("numpy version : ", numpy.__version__)
+print("pandas version : ", pandas.__version__)
+print("matplotlib version : ", matplotlib.__version__)
+print("seaborn version : ", seaborn.__version__)
+print("sklearn version : ", sklearn.__version__)
+
+print(sys.version)
